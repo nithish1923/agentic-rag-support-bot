5c21dc7ac9535bd95283e0d9097199a10fd7eb4a
nithish1923 <63442177+nithish1923@users.noreply.github.com>
2025-06-04 15:27:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ import os
 from sentence_transformers import SentenceTransformer
 from transformers import pipeline
 from langchain.chains import RetrievalQA
-from langchain_community.vectorstores import FAISS
-from langchain_huggingface import HuggingFacePipeline
+from langchain_community.vectorstores.faiss import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.docstore.document import Document
